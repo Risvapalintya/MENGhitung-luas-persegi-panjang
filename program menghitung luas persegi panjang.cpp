91c0afb65cb2003f0756f67ea05633e2198142d2
risva palintya <45660844+Risvapalintya@users.noreply.github.com>
2019-01-04 15:00:24 +0700

    Add files via upload

---
+++ program menghitung luas persegi panjang.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int p,l;
+    cout<<"menghitung luas persegi panjang"<<endl;
+    cout<<"masukan panjang:";
+    cin>>p;
+    cout<<"masukan lebar:";
+    cin>>l;
+    cout<<"luas persegi panjang adalah="<<p*l<<"\n";
+
+    return 0;
+
+}
